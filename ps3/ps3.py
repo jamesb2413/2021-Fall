40bc559853008fc4300b1b71f5de53fcbf4940c9
James Barna <nbarna1298@gmail.com>
2021-09-28 19:31:27 -0400

    com

--- ps3/ps3.py
+++ ps3/ps3.py
@@ -14,7 +14,7 @@ class BinarySearchTree:
     @property
     def size(self):
          return self._size
-       
+
      # a setter function
     @size.setter
     def size(self, a):
@@ -47,7 +47,7 @@ class BinarySearchTree:
 
     '''
     Select the ind-th key in the tree
-    
+
     ind: a number between 0 and n-1 (the number of nodes/objects)
     returns BinarySearchTree/Node or None
     '''
@@ -60,7 +60,7 @@ class BinarySearchTree:
         if left_size > ind and self.left is not None:
             return self.left.select(ind)
         if left_size < ind and self.right is not None:
-            return self.right.select(ind)
+            return self.right.select(ind -(left_size+1))
         return None
 
 
@@ -78,20 +78,22 @@ class BinarySearchTree:
         elif self.left is not None:
             return self.left.search(key)
         return None
-    
+
 
     '''
     Inserts a key into the tree
 
-    key: the key for the new node; 
+    key: the key for the new node;
         ... this is NOT a BinarySearchTree/Node, the function creates one
-    
+
     returns the original (top level) tree - allows for easy chaining in tests
     '''
     def insert(self, key):
         if self.key is None:
             self.key = key
-        elif self.key > key: 
+            return self
+        self.size += 1
+        if self.key > key:
             if self.left is None:
                 self.left = BinarySearchTree(self.debugger)
             self.left.insert(key)
@@ -99,19 +101,93 @@ class BinarySearchTree:
             if self.right is None:
                 self.right = BinarySearchTree(self.debugger)
             self.right.insert(key)
-        self.calculate_sizes()
         return self
 
-    
+
     ####### Problem 2 #######
 
     '''
     Deletes a key from the tree
-    Returns the root of the tree or None if the tree has no nodes   
+    Returns the root of the tree or None if the tree has no nodes
     '''
+    # Unit Test for tree with no nodes, deleting root of tree with one node
+
+
+    def get_pred(self):
+        # Find pred, decrementing along path
+        pred = self.left
+        while pred.right is not None:
+            pred = pred.right
+        return pred
+
+    # void function, swaps data from delete node and predecessor
+    def pred_swap(self, pred):
+        k_i_tmp = self.key, self.item
+        self.key = pred.key
+        self.item = pred.item
+        pred.key = k_i_tmp[0]
+        pred.item = k_i_tmp[1]
+
+    # searches for delete node and decrements sizes on the path
+    # returns delete node, parent tuple
+    def search_dec(self, key, parent):
+        if self.key == key:
+            return self, parent
+        elif key > self.key:
+            self.size -= 1
+            return self.right.search_dec(key, self)
+        else:
+            self.size -= 1
+            return self.left.search_dec(key, self)
+
     def delete(self, key):
-        # Your code goes here
-        pass
+        if self is None:
+            return None
+        if self.search(key) is None:
+            return self
+        # del: tuple (<node to delete>, <parent>)
+        del_node, del_parent = self.search_dec(key, None)
+        # Two children
+        if del_node.left is not None and del_node.right is not None:
+            # Find greatest predecessor and swap data
+            pred_swap(del_node, get_pred(del_node))
+            # Delete swapped node
+            delete(del_node, key)
+        else:
+            # delete node is the root
+            if del_parent is None:
+                # No children
+                if del_node.left is None and del_node.right is None:
+                    self = None
+                # Left child
+                elif del_node.right is None and del_node.left is not None:
+                    self = del_node.left
+                # Right child
+                else:
+                    self = del_node.right
+            # delete node is left subtree of parent
+            elif del_parent.left is del_node:
+                # No children
+                if del_node.left is None and del_node.right is None:
+                    del_parent.left = None
+                # Left child
+                elif del_node.right is None and del_node.left is not None:
+                    del_parent.left = del_node.left
+                # Right child
+                else:
+                    del_parent.left = del_node.right
+            # delete node is right subtree of parent
+            else:
+                # No children
+                if del_node.left is None and del_node.right is None:
+                    del_parent.right = None
+                # Left child
+                elif del_node.right is None and del_node.left is not None:
+                    del_parent.right = del_node.left
+                # Right child
+                else:
+                    del_parent.right = del_node.right
+        return self
 
     '''
     Performs a `direction`-rotate the `side`-child of (the root of) T (self)
@@ -129,7 +205,7 @@ class BinarySearchTree:
         11
           \
            12
-    
+
     Execute: NodeFor10.rotate("R", "L") -> Outputs: NodeFor10
 
     Output Graph
@@ -137,10 +213,82 @@ class BinarySearchTree:
         \
         12
         /
-       11 
+       11
     '''
+    def left_rotate(self):
+        # save relevant nodes and sizes
+        left = self
+        rootSize = self.size
+        llExists = 0
+        lrExists = 0
+        if self.left is not None:
+            llSize = self.left.size
+            llExists = 1
+        if self.right.left is not None:
+            leftRight = self.right.left
+            lrSize = leftRight.size
+            lrExists = 1
+        # set pointers and adjust sizes
+        self = self.right
+        self.size = rootSize
+        self.left = left
+        if lrExists and llExists:
+            self.left.right = leftRight
+            self.left.size = llSize + lrSize + 1
+        elif lrExists:
+            self.left.right = leftRight
+            self.left.size = lrSize + 1
+        elif llExists:
+            self.left.size = llSize + 1
+        else:
+            self.left.size = 1
+        return self
+
+    def right_rotate(self):
+        # save relevant nodes and sizes
+        right = self
+        rootSize = self.size
+        rrExists = 0
+        rlExists = 0
+        if self.right is not None:
+            rrSize = self.right.size
+            rrExists = 1
+        if self.left.right is not None:
+            rightLeft = self.left.right
+            rlSize = rightLeft.size
+            rlExists = 1
+        # set pointers and adjust sizes
+        self = self.left
+        self.size = rootSize
+        self.right = right
+        if rlExists and rrExists:
+            self.right.left = rightLeft
+            self.right.size = rrSize + rlSize + 1
+        elif rlExists:
+            self.right.left = rightLeft
+            self.right.size = rlSize + 1
+        elif rrExists:
+            self.right.size = rrSize + 1
+        else:
+            self.right.size = 1
+        return self
+
+    # Return tree with rotated node as root
     def rotate(self, direction, child_side):
-        # Your code goes here
+        if direction == 'L':
+            # left-rotate left-child
+            if child_side == 'L':
+                self.left = self.left.left_rotate()
+            # left-rotate right-child
+            else:
+                self.right = self.right.left_rotate()
+        else:
+            # right-rotate left-child
+            if child_side == 'L':
+                self.left = self.left.right_rotate()
+            # right-rotate right-child
+            else:
+                self.right = self.right.right_rotate()
         return self
 
     def print_bst(self):
@@ -150,4 +298,3 @@ class BinarySearchTree:
         if self.right is not None:
             self.right.print_bst()
         return self
-

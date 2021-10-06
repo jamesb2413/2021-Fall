b291cdf9b5f8a761c752e7dd79698ec4e030d5bb
James Barna <nbarna1298@gmail.com>
2021-10-06 16:41:20 -0400

    hm

--- ps4/simulator.py
+++ ps4/simulator.py
@@ -5,9 +5,9 @@
 from collections import defaultdict
 
 variableList = []
-# Note: defaultdict works exactly the same as a normal Python dictionary except it returns a default 
+# Note: defaultdict works exactly the same as a normal Python dictionary except it returns a default
 #       value (in this case, 0) when accessing a key that is not defined rather than raising KeyError.
-#       We are using a dictionary rather than a list/array to manage the memory so that we don't need to 
+#       We are using a dictionary rather than a list/array to manage the memory so that we don't need to
 #       initialize and store memory cells that are never accessed by the RAM program.
 memory = defaultdict(int)
 
@@ -19,34 +19,34 @@ def setupEnv(programArr, inputArr):
 
     for i in range(programArr[0]):
         variableList.append(0)
-    
+
     variableList[0] = len(inputArr)
     for i in range(len(inputArr)):
         memory[i] = inputArr[i]
-        
+
 # Runs the given RAM program on the input.
 def executeProgram(programArr, inputArr):
     setupEnv(programArr, inputArr)
-    
+
     programArr = programArr[1:]
     programCounter = 0
     while programCounter < len(programArr):
         # Store the command and the list of operands.
         cmd = programArr[programCounter][0]
         ops = programArr[programCounter][1:]
-        
+
         # Assignment commands
-        if cmd == "read":       
-            # ['read', i, j]: lookup the var_j location in memory and assign that value to var_i                    
+        if cmd == "read":
+            # ['read', i, j]: lookup the var_j location in memory and assign that value to var_i
             variableList[ops[0]] = memory[variableList[ops[1]]]
         if cmd == "write":
-            # ['write', i, j]: store the value of var_j in memory at the location var_i 
+            # ['write', i, j]: store the value of var_j in memory at the location var_i
             memory[variableList[ops[0]]] = variableList[ops[1]]
         if cmd == "assign":
             # ['assign', i, j]: assign var_i to the value j
             # TODO: Implement assign.
             pass
-            
+
         # Arithmetic commands
         if cmd == "+":
             # ['+', i, j, k]: compute (var_j + var_k) and store in var_i
@@ -65,7 +65,7 @@ def executeProgram(programArr, inputArr):
             # Note that this is integer division. You should return an integer, not a float.
             # TODO: Implement division.
             pass
-            
+
         # Control commands
         if cmd == "halt":
             # ['halt']: halt the program.
@@ -75,8 +75,8 @@ def executeProgram(programArr, inputArr):
             # ['goto', i, j]: if var_i is equal to 0, go to line j
             # TODO: Implement goto.
             pass
-        
+
         programCounter += 1
-    
+
     # Return the memory starting at output_ptr with length of output_len
     return [memory[i] for i in range(variableList[1], variableList[1]+variableList[2])]
